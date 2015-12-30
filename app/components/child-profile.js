9d6309ecbd34fef8ea8acd0e17ac4f5d24185074
Adit Trivedi <adittrivedi1993@gmail.com>
2015-12-30 21:37:46 +0000

    registration complete

--- app/components/child-profile.js
+++ app/components/child-profile.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import utilities from '../utilities/utilities';
 
 export default Ember.Component.extend({
+	childId: '',
 	firstName: '',
 	lastName: '',
 	genderIndex: 0,
@@ -20,15 +21,21 @@ export default Ember.Component.extend({
 			var userDOB = this.get('dob');
 			this.set('dateOfBirth',userDOB.toDateString());
 		}
-		
-		
 	}.observes('dob'),
+
+	didInsertElement: function() {
+		//check if parent has a profile and fill in their details
+		this.triggerAction({
+			action:'getProfileDetails',
+			target: this
+		});
+	},
 
 	genders: [
 		{name: ''},
-		{name: 'male'},
-		{name: 'female'},
-		{name: 'prefer not to say'},
+		{name: 'Male'},
+		{name: 'Female'},
+		{name: 'Prefer not to say'},
 	],
 
 	actions:{
@@ -37,7 +44,9 @@ export default Ember.Component.extend({
 			var lastName = this.get('lastName');
 			var gender = this.get('gender.name');
 			var dob = this.get('dob');
+			//strDob just used to check length as is a string object
 			var strDob= this.get('dateOfBirth');
+			var self = this
 			if(utilities.lengthChecker(firstName,0)&&utilities.lengthChecker(lastName,0)&&
 				utilities.lengthChecker(gender,0) &&utilities.lengthChecker(strDob,0)){
 				var user = Parse.User.current();
@@ -49,16 +58,52 @@ export default Ember.Component.extend({
 					"dateOfBirth": dob,
 					"parent": user
 				});
-				profile.save(null,{
-					success: function(result){
-						console.log("saved child profile successfully");
-					},
-					error: function(error){
-						console.log("Error saving child profile: " +error.message)
-					}
-				});
+				var childId = self.get('childId');
+				if(childId.length>0){
+					var query = new Parse.Query("ChildProfiles");
+					query.equalTo("objectId", childId);
+					query.first({
+						success: function(results){
+							console.log(results.get("firstName"));
+							results.set("firstName", firstName);
+							results.set("lastName", lastName);
+							results.set("gender", gender);
+							results.set("dateOfBirth", dob);
+							results.save();
+							console.log("Successfully edited results");
+						}
+					});
+				} else{
+					profile.save(null,{
+						success: function(result){
+							console.log("saved child profile successfully");
+						},
+						error: function(error){
+							console.log("Error saving child profile: " +error.message);
+						}
+					});
+				}
 			}
+		},
 
+		'getProfileDetails': function(){
+			console.log("child profile reached");
+			var self = this;
+			var ChildProfile = Parse.Object.extend("ChildProfiles");
+			var q = new Parse.Query(ChildProfile);
+			var user = Parse.User.current();
+			q.equalTo("parent",user);
+			q.find({
+			  success: function(results){
+			  	for(var i = 0; i<results.length; i++){
+			  		self.set('childId', results[i].id);
+			  		self.set('firstName', results[i].get('firstName'));
+			  		self.set('lastName', results[i].get('lastName'));
+			  		self.set('gender.name', results[i].get('gender'));
+			  		self.set('dob', results[i].get('dateOfBirth'));
+			  	}
+			  }
+			});
 		}
 	}
 });
--- app/components/parent-profile.js
+++ app/components/parent-profile.js
@@ -4,10 +4,16 @@ import utilities from '../utilities/utilities';
 export default Ember.Component.extend({
 	firstName: '',
 	lastName: '',
+	parentId:'',
+	genderIndex: 0,
 	languageIndex: 0,
 
 	nationality: Ember.computed('languageIndex', function() {
 		return this.get('languages')[this.get('languageIndex')];
+	}),
+
+	gender: Ember.computed('genderIndex', function() {
+		return this.get('genders')[this.get('genderIndex')];
 	}),
 
 	languages: [
@@ -19,11 +25,28 @@ export default Ember.Component.extend({
 		{name: 'Spanish'}
 	],
 
+	genders: [
+		{name: ''},
+		{name: 'Male'},
+		{name: 'Female'},
+		{name: 'Prefer not to say'},
+	],
+
+	didInsertElement: function() {
+		//check if parent has a profile and fill in their details
+		this.triggerAction({
+			action:'getProfileDetails',
+			target: this
+		});
+	},
+
 	actions:{
 		'submitParentProfile': function(){
 			var firstName = this.get('firstName');
 			var lastName = this.get('lastName');
 			var nationality = this.get('nationality.name');
+			var gender = this.get('gender.name');
+			var self = this;
 			if(utilities.lengthChecker(firstName,0)&&utilities.lengthChecker(lastName,0)&&
 				utilities.lengthChecker(nationality,0)){
 				var user = Parse.User.current();
@@ -32,17 +55,55 @@ export default Ember.Component.extend({
 					"firstName":firstName,
 					"lastName":lastName,
 					"nationality": nationality,
+					"gender": gender,
 					"user": user
 				});
-				profile.save(null,{
-					success: function(result){
-						console.log("saved parent profile successfully");
-					},
-					error: function(error){
-						console.log("Error saving parent profile: " +error.message)
-					}
-				});
+				var parentId = self.get('parentId');
+				if(parentId.length>0){
+					var query = new Parse.Query("ParentProfile");
+					query.equalTo("objectId", parentId);
+					query.first({
+						success: function(results){
+							console.log(results.get("firstName"));
+							results.set("firstName", firstName);
+							results.set("lastName", lastName);
+							results.set("nationality", nationality);
+							results.set("gender", gender);
+							results.save();
+							console.log("Successfully edited results");
+						}
+					});
+				} else{
+					profile.save(null,{
+						success: function(result){
+							console.log("saved parent profile successfully");
+						},
+						error: function(error){
+							console.log("Error saving parent profile: " +error.message)
+						}
+					});
+				}
+				
 			}
+		},
+
+		'getProfileDetails': function(){
+			var self = this;
+			var ParentProfile = Parse.Object.extend("ParentProfile");
+			var q = new Parse.Query(ParentProfile);
+			var user = Parse.User.current();
+			q.equalTo("user",user);
+			q.find({
+			  success: function(results){
+			  	for(var i = 0; i<results.length; i++){
+			  		self.set('parentId', results[i].id);
+			  		self.set('firstName', results[i].get('firstName'));
+			  		self.set('lastName', results[i].get('lastName'));
+			  		self.set('nationality.name', results[i].get('nationality'));
+			  		self.set('gender.name', results[i].get('gender'));
+			  	}
+			  }
+			});
 		}
 	}
 });
