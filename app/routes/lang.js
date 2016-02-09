a2b05ae043d59a854113acceb3e2a7b52aa57462
Toby Mather <tobymather@gmail.com>
2016-02-09 14:09:29 +0000

    adding some changes to switcher, stuff working here

---
+++ app/routes/lang.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import config from '../config/environment';
+
+export default Ember.Route.extend({
+	// afterModel: function (params){
+ //        var allowedLocales = config.i18n.allowedLocales;
+ //        var defaultLocale = config.i18n.defaultLocale;
+ //        this.set(
+ //            'i18n.locale',
+ //            params && params.lang && allowedLocales.indexOf(params.lang) > -1 ? params.lang : defaultLocale
+ //        );
+ //    }
+});
